0e40d5ccfc9bc1c2c3d3166103a6891ae961761f
DenisTheo <contact.denis.theo@gmail.com>
2023-10-10 10:56:34 +0200

    cleaned code

--- src/fr/diginamic/essais/testForme.java
+++ src/fr/diginamic/essais/testForme.java
@@ -6,7 +6,6 @@ public class testForme
 {
 	public static void main(String[] args)
 	{
-		
 		AffichageForme.afficher(new Cercle(6.66));
 		AffichageForme.afficher(new Rectangle(62, 51));
 		AffichageForme.afficher(new Carre(42));
