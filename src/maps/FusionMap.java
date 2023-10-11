2d9bc2d03dcf43add5731530446ec0f3c044de3f
DenisTheo <contact.denis.theo@gmail.com>
2023-10-11 17:01:18 +0200

    initialised exercise 4

---
+++ src/maps/FusionMap.java
@@ -0,0 +1,19 @@
+package maps;
+
+import java.util.HashMap;
+
+public class FusionMap
+{
+	public static void main(String args[])
+	{
+		HashMap<Integer, String> map1 = new HashMap<Integer, String>();
+		map1.put(1, "Rouge");
+		map1.put(2, "Vert");
+		map1.put(3, "Orange");
+		
+		HashMap<Integer, String> map2 = new HashMap<Integer, String>();
+		map2.put(4, "Blanc");
+		map2.put(5, "Bleu");
+		map2.put(6, "Orange");
+	}
+}
