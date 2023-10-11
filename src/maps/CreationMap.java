9d738cc60748ac41e53a807c40acb09b28bec26d
DenisTheo <contact.denis.theo@gmail.com>
2023-10-11 16:45:41 +0200

    fixed var name in last exercise + initialised exercise 2

--- src/maps/CreationMap.java
+++ src/maps/CreationMap.java
@@ -7,20 +7,20 @@ public class CreationMap
 {
 	public static void main(String[] args)
 	{
-		Map<String, Integer> salaries = new HashMap<>();
+		Map<String, Integer> mapSalaires = new HashMap<>();
 
-		salaries.put("Paul", 1_750);
-		salaries.put("Hicham", 1_825);
-		salaries.put("Yu", 2_250);
-		salaries.put("Ingrid", 2_015);
-		salaries.put("Chantal", 2_418);
+		mapSalaires.put("Paul", 1_750);
+		mapSalaires.put("Hicham", 1_825);
+		mapSalaires.put("Yu", 2_250);
+		mapSalaires.put("Ingrid", 2_015);
+		mapSalaires.put("Chantal", 2_418);
 
-		for (Map.Entry<String, Integer> employee : salaries.entrySet())
+		for (Map.Entry<String, Integer> employee : mapSalaires.entrySet())
 		{
             System.out.println("Clé : " + employee.getKey() + ", Valeur : " + employee.getValue());
         }
 		
-		System.out.println("Size: " + salaries.size());
+		System.out.println("Size: " + mapSalaires.size());
 
 	}
 }
