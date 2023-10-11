0e8a35c14cac23f5258dc3628ce9315c174eea1d
DenisTheo <contact.denis.theo@gmail.com>
2023-10-11 14:06:04 +0200

    List exercise 4 complete

--- src/listes/TestListString.java
+++ src/listes/TestListString.java
@@ -1,5 +1,51 @@
 package listes;
 
-public class TestListString {
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Iterator;
 
+public class TestListString
+{
+	public static void main(String[] args)
+	{
+		List<String> list = new ArrayList<String>();
+		list.add("Nice");
+		list.add("Carcassone");
+		list.add("Narbonne");
+		list.add("Lyon");
+		list.add("Foix");
+		list.add("Pau");
+		list.add("Marseille");
+		list.add("Tarbes");
+		
+		String target = "";
+		
+		Iterator<String> iterator = list.iterator();
+		
+		while (iterator.hasNext())
+		{
+            String next = iterator.next();
+            if (next.length() > target.length())
+                target = next;
+        }
+		System.out.println(target);
+
+		iterator = list.iterator(); // reset
+		while (iterator.hasNext())
+		{
+            String next = iterator.next();
+            list.set(list.indexOf(next), next.toUpperCase());
+        }
+
+		iterator = list.iterator(); // reset
+		while (iterator.hasNext())
+		{
+            String next = iterator.next();
+            if (next.startsWith("N"))
+            	iterator.remove();
+        }
+		
+		System.out.println(list.toString());
+		
+	}
 }
